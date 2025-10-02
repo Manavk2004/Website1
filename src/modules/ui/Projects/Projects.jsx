8e40f8fca376efdd28c43e4cc60db26cde7ad277
Manavk2004 <mjkamdar04@gmail.com>
2025-10-02 14:24:09 -0400

    MUSAI link

--- src/modules/ui/Projects/Projects.jsx
+++ src/modules/ui/Projects/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <div className='bg-transparent! h-fit w-full grid grid-cols-13 auto-rows p-4'>
       <ProjectCard title="AI SaaS " description="AI Powered Customer Support Application " class="border-0! col-start-6 col-end-9 row-start-2 row-end-7" image="/saas.png" name="Echo"/>
-      <ProjectCard title="MUSAI" description="AI Powered Music Recommendation App" class="border-0! col-start-2 col-end-5 row-start-2 row-end-7" image="/musai.png" name="MUSAI" url="musai.onrender.com/login-page"/>
+      <ProjectCard title="MUSAI" description="AI Powered Music Recommendation App" class="border-0! col-start-2 col-end-5 row-start-2 row-end-7" image="/musai.png" name="MUSAI" url="https://musai.onrender.com/login-page"/>
       <ProjectCard title="Nimbus KeyBoards" description="3D E-Commerce Website" class="border-0! col-start-10 col-end-13 row-start-2 row-end-7" image="/nimbus.png" name="Nimbus" url="https://3-d-keyboard-xqgl.vercel.app/"/>
     </div>
   )
